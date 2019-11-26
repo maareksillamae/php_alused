75d987718bb596aa48ce196e4f3980e6a2990239
Maarek <Maarek.sillamae@khk.ee>
2019-11-26 16:53:03 +0200

    maailal6pp

---
+++ arrays/tervitus.php
@@ -0,0 +1,17 @@
+<?php
+
+//Koosta kood, mis tervitab sind vastavalt ajale. N: 8:00+ “Tere hommikust!”, 13:00+ “Tere päevast!” ja 17:00+ “Tere õhtust!”
+$hour = date('H', time());
+
+if( $hour > 8 && $hour <= 13) {
+    echo "Tere hommikust!";
+}
+else if($hour > 13 && $hour <= 17) {
+    echo "Tere p2evast!";
+}
+else if($hour > 17 && $hour <= 20) {
+    echo "Tere 6htust!";
+}
+else {
+    echo "miks sa ei maga? Kas sa j2lle proged?";
+}
