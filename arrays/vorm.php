a4e6a40efd12572e0e0e5a460e35418c32550948
Maarek <Maarek.sillamae@khk.ee>
2019-11-26 08:46:21 +0200

    Alg vorm + tellimine yhendamine

---
+++ arrays/vorm.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>06 - PHP - Vormid</title>
+</head>
+<body>
+    <h1>Pood OÜ</h1>
+    <form action="tellimine.php" method="get">
+Toode 1 <input type="text"><br>
+Toode 2 <input type="text"><br>
+Toode 3 <input type="text"><br>
+        <input type="submit" value="Saada">
+    </form>
+</body>
+</html>
