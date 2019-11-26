fda1b02b8062bdde4020c6b440d85cd64b29d1c8
Maarek <Maarek.sillamae@khk.ee>
2019-11-26 09:06:08 +0200

    vormi t66tlus + get vormi andmed

--- arrays/tellimine.php
+++ arrays/tellimine.php
@@ -1,3 +1,18 @@
 <?php
+
 ?>
 <h1>Tellimine</h1>
+<?php
+
+//var_dump($_GET);
+  //  echo"<pre>";
+   // print_r($_GET);
+   // echo "</pre>";
+
+$toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
+
+echo 'Toode 1: '.$toode1.'tk<br>';
+echo 'Toode 2: '.$toode2.'tk<br>';
+echo 'Toode 3: '.$toode3.'tk<br>';
