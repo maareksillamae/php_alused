d65b9596345ec8d44126963ff687dbcc2e4de82b
Maarek <Maarek.sillamae@khk.ee>
2019-11-26 09:12:04 +0200

    koonus silindre jne

---
+++ arrays/arvutused.php
@@ -0,0 +1,23 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>06 - PHP - Vormid</title>
+</head>
+<body>
+    <h1>Arvutused</h1>
+    <form action="arvutused.php" method="get">
+        <h2>kera</h2>
+        kera raadius <input type="text" name="kera-raadius"><br>
+        <h2>koonus</h2>
+        koonuse p6hjaraadius <input type="text" name="koonuse-raadius"><br>
+        koonuse k6rgus <input type="text" name="koonuse-k6rgus"><br>
+
+<h2>silindre</h2>
+        silindri p6hjaraadius <input type="text" name="silindri-raadius"><br>
+        silindri k6rgus <input type="text" name="silindri-k6rgus"><br>
+        <input type="submit" value="Saada">
+    </form>
+</body>
+</html>
