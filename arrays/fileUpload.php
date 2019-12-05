0f4dc17d962784cd6b1094e66c3f5f1a8dff6cc2
Maarek <Maarek.sillamae@khk.ee>
2019-12-05 12:14:33 +0200

    jpg ja jpeg yles laadimine (poolik veel kuna millegi p2rast annab errori kui laadida yles pilt )

---
+++ arrays/fileUpload.php
@@ -0,0 +1,39 @@
+<?php
+$currentDir = getcwd();
+$uploadDirectory = "/uploads/";
+
+$errors = []; // Store all foreseen and unforseen errors here
+
+$fileExtensions = ['jpeg','jpg']; // Get all the file extensions
+
+$fileName = $_FILES['myfile']['name'];
+$fileSize = $_FILES['myfile']['size'];
+$fileTmpName  = $_FILES['myfile']['tmp_name'];
+$fileType = $_FILES['myfile']['type'];
+$fileExtension = strtolower(end(explode('.',$fileName)));
+
+$uploadPath = $currentDir . $uploadDirectory . basename($fileName);
+
+if (isset($_POST['submit'])) {
+
+    if (! in_array($fileExtension,$fileExtensions)) {
+        $errors[] = "This file extension is not allowed. Please upload a JPEG or JPG file";
+    }
+
+    if (empty($errors)) {
+        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+        if ($didUpload) {
+            echo "The file " . basename($fileName) . " has been uploaded";
+        } else {
+            echo "An error occurred somewhere. Try again or contact the admin";
+        }
+    } else {
+        foreach ($errors as $error) {
+            echo $error . "<br>These are the errors" . "\n";
+        }
+    }
+}
+
+
+?>
