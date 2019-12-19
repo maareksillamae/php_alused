24d52f6bb59806645e20aac1c6b08fce92cfd48c
Maarek <Maarek.sillamae@khk.ee>
2019-12-19 12:38:38 +0200

    session login

---
+++ session/index.php
@@ -0,0 +1,4 @@
+<?php
+echo '<a href="add.php">Registreeri kasutajaks</a>';
+echo '&nbsp;&nbsp;&nbsp;';
+echo '<a href="login.php">Logi sisse</a>';
---
+++ session/login.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config.php';
+require_once 'db_fnc.php';
+$ikt = connection(HOSTNAME, USERNAME, PASSWORD, DBNAME);
+// kui vormi kaudu on andmed saadetud
+if(!empty($_POST['nimi']) and !empty($_POST['parool'])){
+    $nimi = $_POST['nimi'];
+    $parool = $_POST['parool'];
+    // kontrollime kasutaja andmebaasis
+    $sql = 'SELECT * FROM kasutajad WHERE nimi="'.$nimi.'" AND parool="'.md5($parool).'"';
+    $result = getData($sql, $ikt);
+    // kui selline kasutaja on olemas andmebaasis
+    if($result !== false){
+        // avame kasutajale sessioon
+        session_start();
+        $_SESSION['kasutaja'] = $result[0]['nimi'];
+        header('Location: index.php');
+    }
+} else {
+    // väljasta vorm
+    echo
+    '<form method="post">
+    Kasutajanimi: <input type="text" name="nimi"><br>
+    Parool: <input type="password" name="parool"><br>
+    <input type="submit" value="Logi sisse">
+    <hr>
+  </form>';
+}
